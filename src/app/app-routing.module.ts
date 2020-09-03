5fa1b46bace41ba6bbae2fb454d9495d5206d0f1
ethantanen <ethantanen@yahoo.com>
2020-09-03 14:45:25 -0400

    oy!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutMeComponent } from './about-me/about-me.component';
+import { EmailMeComponent } from './email-me/email-me.component';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+
+const routes: Routes = [
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'email-me', component: EmailMeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: '**', redirectTo: '/home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
+import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { CircleComponent } from './circle/circle.component';
+import { CirclesComponent } from './circles/circles.component';
+import { EmailMeComponent } from './email-me/email-me.component';
+import { HomeComponent } from './home/home.component';
+import { MessageComponent } from './message/message.component';
+import { NavComponent } from './nav/nav.component';
+import { ResumeComponent } from './resume/resume.component';
+
+import { EmailService } from './email.service';
+import { MessageService } from './message.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    CircleComponent,
+    CirclesComponent,
+    EmailMeComponent,
+    HomeComponent,
+    MessageComponent,
+    NavComponent,
+    ResumeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
+  providers: [EmailService, MessageService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
