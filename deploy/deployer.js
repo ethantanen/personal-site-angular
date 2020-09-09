76d491466a6a337df81cbb9c04bb1742e9fabe72
ethantanen <ethantanen@yahoo.com>
2020-09-09 13:36:12 -0400

    add body parser

--- deploy/deployer.js
+++ deploy/deployer.js
@@ -1,19 +1,21 @@
 /**
 	1) listen for "deploy" tag from github webhook
-	2) pull dist directory  
-	3) restart nginx 
+	2) pull dist directory
+	3) restart nginx
 */
 
 const express = require('express');
+const bodyParser = require('body-parser')
 const shell = require('shelljs');
 const cors = require('cors');
 
 const app = express();
 
-app.post('/deploy', async (req, res) => {
-const f =	shell.exec('git checkout origin -- dist')
-console.log(JSON.stringify(f, null, 2))
-res.status(200).end()
+app.use(bodyParser.json())
+app.post('/', async (req, res) => {
+  // const f = shell.exec('git checkout origin/develop -- dist')
+  console.log(JSON.stringify(req.body, null, 2))
+  res.status(200).end()
 
 
 })
