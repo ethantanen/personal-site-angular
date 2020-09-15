78ccfc7c85a11f493809ed637ce000cbe3fb1339
EC2 Default User <ec2-user@ip-10-0-0-13.ec2.internal>
2020-09-15 19:03:52 +0000

    update auto deploy feature

--- deploy/deployer.js
+++ deploy/deployer.js
@@ -1,19 +1,15 @@
-/**
-	1) listen for "deploy" tag from github webhook
-	2) pull dist directory
-	3) restart nginx
-*/
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const shell = require("shelljs");
 const cors = require("cors");
+const { execSync } = require('child_process');
 
 const DEPLOY_BRANCH = "master";
 
 const app = express();
 
 app.use(bodyParser.json());
+
 app.post("/", async (req, res) => {
   const {
     action,
@@ -22,11 +18,13 @@ app.post("/", async (req, res) => {
       merged,
     },
   } = req.body;
-
   if (merged && baseBranchName === DEPLOY_BRANCH) {
-    console.log("DEPLOY ME!!!!");
+    console.log(`Deploying changes from ${baseBranchName}...`);
+    const stdout = execSync('sh deploy.sh');
+    console.log(stdout.toString('utf8'))
+    console.log('Finished Deploying!\n')
   }
   res.status(200).end();
 });
 
-app.listen(8080);
+app.listen(8081, () => console.log('listening on 8081'));
